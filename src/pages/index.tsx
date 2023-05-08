fa21aa8be875990784a9690644832f28c669968a
Giovanni Panzetta <giodevcoding@gmail.com>
2023-05-08 11:36:37 -0500

    new db stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser()
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -18,10 +18,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <p className="text-2xl text-white">
+        <div className="text-2xl text-white">
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <UserButton /> }
-        </p>
+        </div>
+        <div>
+          {data?.map(post => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
