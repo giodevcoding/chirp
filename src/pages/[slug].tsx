c7199e21d98ac733691700c04a527cac756217fa
Giovanni Panzetta <giodevcoding@gmail.com>
2023-05-19 13:26:12 -0500

    Render posts on profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -11,34 +11,53 @@ import { prisma } from "~/server/db";
 import { api } from "~/utils/api";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postview";
+
+const ProfileFeed = (props: {userId: string}) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({userId: props.userId})
+
+  if (isLoading) return <LoadingPage />
+
+  if (!data || !data.length) return <div>User has not posted...</div>
+
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  )
+}
 
 type ProfilePageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 const ProfilePage: NextPage<ProfilePageProps> = ({ username }) => {
-  const { data } = api.profile.getUserByUsername.useQuery({
+  const { data: userData } = api.profile.getUserByUsername.useQuery({
     username,
   });
 
-  if (!data) return <div>404</div>;
+  if (!userData) return <div>404</div>;
 
   return (
     <>
       <Head>
-        <title>{data.username}</title>
+        <title>{userData.username}</title>
       </Head>
       <PageLayout>
         <div className="relative h-36 bg-slate-600">
           <Image
-            src={data.profileImageUrl}
-            alt={`${data.username ?? ""}'s profile picture`}
+            src={userData.profileImageUrl}
+            alt={`${userData.username ?? ""}'s profile picture`}
             width={128}
             height={128}
             className="absolute bottom-0 left-0 -mb-16 ml-4 rounded-full border-4 border-black"
           />
         </div>
         <div className="h-16"></div>
-        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
+        <div className="p-4 text-2xl font-bold">{`@${userData.username ?? ""}`}</div>
         <div className="border-b border-slate-400 w-full"></div>
+        <ProfileFeed userId={userData.id}/>
       </PageLayout>
     </>
   );
